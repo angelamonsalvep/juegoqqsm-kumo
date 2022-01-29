3a53ea357d90a7d329fbccef2bf867eaae938892
Angela Monsalve <angelamonsalve@wposs.com>
2022-01-29 14:33:51 -0500

    configuración inicial parte 2

--- config/mongo.js
+++ config/mongo.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose')
+
+const dbConnect = () => {
+    const DB_URI = process.env.DB_URI
+    mongoose.connect(DB_URI, {
+        useNewUrlparser: true,
+        useUnifiedTopology: true
+    })
+}
+
+module.exports = { dbConnect }
